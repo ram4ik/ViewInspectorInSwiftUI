70337ff3818f3cf5d68db5c668c736041f52c33d
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-02-01 13:52:02 +0200

    Initial Commit

---
+++ ViewInspectorInSwiftUI/ViewInspectorInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewInspectorInSwiftUIApp.swift
+//  ViewInspectorInSwiftUI
+//
+//  Created by ramil on 01.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct ViewInspectorInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
